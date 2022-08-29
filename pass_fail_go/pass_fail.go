2c34c42313379f71a72ccc4c2e3ec15cf55e1cce
James C. Tucker <hellojamestucker@gmail.com>
2022-08-29 08:54:42 -0500

    fix var scoping error

--- pass_fail_go/pass_fail.go
+++ pass_fail_go/pass_fail.go
@@ -14,20 +14,22 @@ func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	input = strings.TrimSpace(input)
-	grade, err = strconv.ParseFloat(input, 64)
+	grade, err := strconv.ParseFloat(input, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	var status string
 	if grade >= 60 {
-		status := "passing"
+		status = "passing"
 	} else {
-		status := "failing"
+		status = "failing"
 	}
 	fmt.Println("A grade of", grade, "is", status)
 }
