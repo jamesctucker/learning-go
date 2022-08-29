1d22aaaf5b37a0e97e7e763b99a4ba84a469c9b3
James C. Tucker <hellojamestucker@gmail.com>
2022-08-29 09:39:30 -0500

    only allow numbers in user input

--- pass_fail_go/pass_fail.go
+++ pass_fail_go/pass_fail.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -14,12 +15,20 @@ func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	input = strings.TrimSpace(input)
+
+	match, _ := regexp.MatchString("^[0-9]*$", input)
+	if !match {
+		fmt.Println("Sorry, you can only enter numeric digits. Please try again.")
+		// TODO: figure out why the initial input is still stored even after recursively running the function again and setting input to a new value
+		reader.Reset(reader)
+		main()		
+	}
+
 	grade, err := strconv.ParseFloat(input, 64)
 	if err != nil {
 		log.Fatal(err)
