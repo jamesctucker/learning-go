f4120be32b813576aedf86fc069a58aac894e888
James C. Tucker <hellojamestucker@gmail.com>
2022-08-31 07:56:11 -0500

    add guessing func

--- guess/guess.go
+++ guess/guess.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	res := genRandomNumber()
-	fmt.Println("I've chosen a random number between 1 and 100.")
-	fmt.Println("Can you guess it?")
-	fmt.Println(res)
+	target := genRandomNumber()
+	guess(target)
 }
 
 func genRandomNumber() int {
 	seconds := time.Now().Unix()
 	rand.Seed(seconds)
 	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
 
 	return target
+}
+
+func guess(target int) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Make a guess: ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if guess < target {
+		fmt.Println("Oops! Your guess was LOW.")
+	} else if guess > target {
+		fmt.Println("Oops! Your guess was HIGH.")
+	}
 }
