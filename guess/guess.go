f0471b490b17131a2b5b8c149cfad049b546a4aa
James C. Tucker <hellojamestucker@gmail.com>
2022-08-31 07:43:16 -0500

    move num generator to separate func

--- guess/guess.go
+++ guess/guess.go
@@ -7,10 +7,16 @@ import (
 )
 
 func main() {
+	res := genRandomNumber()
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+	fmt.Println(res)
+}
+
+func genRandomNumber() int {
 	seconds := time.Now().Unix()
 	rand.Seed(seconds)
 	target := rand.Intn(100) + 1
-	fmt.Println("I've chosen a random number between 1 and 100.")
-	fmt.Println("Can you guess it?")
-	fmt.Println(target)
+
+	return target
 }
