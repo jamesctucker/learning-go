add3bae833091921cb641bd6361d6eb8f4c228ac
James C. Tucker <hellojamestucker@gmail.com>
2022-09-04 13:12:47 -0500

    add error handling

--- paint_calculator/paint_calculator.go
+++ paint_calculator/paint_calculator.go
@@ -1,13 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	calculatePaintNeeded(4.2, 3.0)
-	calculatePaintNeeded(5.2, 3.5)
+	var total float64
+	amount, err := calculatePaintNeeded(4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2f liters needed\n", amount)
+	total += amount
+
+	amount, err = calculatePaintNeeded(5.2, 3.5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2f liters needed\n", amount)
+	total += amount
+	fmt.Printf("Total: %.2f liters needed\n", total)
 }
 
-func calculatePaintNeeded(width float64, height float64) {
+func calculatePaintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
+
 	area := width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	return area / 10.0, nil
 }
