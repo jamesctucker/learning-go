4c834a1a7149afa23d084884c4c8b66e15c4e223
James C. Tucker <hellojamestucker@gmail.com>
2022-09-04 12:53:10 -0500

    move repeated logic to func

--- paint_calculator/paint_calculator.go
+++ paint_calculator/paint_calculator.go
@@ -3,14 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var width, height, area float64
-	width = 4.2
-	height = 3.0
-	area = width * height
-	fmt.Println(area/10.0, "liters needed")
+	calculatePaintNeeded(4.2, 3.0)
+	calculatePaintNeeded(5.2, 3.5)
+}
 
-	width = 5.2
-	height = 3.5
-	area = width * height
-	fmt.Println(area/10.0, "liters needed")
+func calculatePaintNeeded(width float64, height float64) {
+	area := width * height
+	fmt.Printf("%.2f liters needed\n", area/10.0)
 }
